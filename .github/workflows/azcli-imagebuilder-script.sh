788176205092c4385b37577c334694ff8321471b
Lalitha Nochili <lanochil@microsoft.com>
2020-04-11 00:36:38 +0530

    Delete azcli-imagebuilder-script.sh

--- .github/workflows/azcli-imagebuilder-script.sh
+++
@@ -1,7 +0,0 @@
-####Create Image Template Resource###
-az resource create  --resource-group ${{resource-group}}  --properties  @${{aib-template-file}} \
-    --is-full-object  --resource-type Microsoft.VirtualMachineImages/imageTemplates -n ${{aib-templage-name}}
-
-#### Run Image Template Resource ###
-az resource invoke-action  --resource-group ${{resource-group}} --resource-type  Microsoft.VirtualMachineImages/imageTemplates \
-      -n ${{image-template-name}} --action Run
